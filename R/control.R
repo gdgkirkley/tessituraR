dcff176a7a28ee73c68f82623d25d15943f78cf5
Gabriel Kirkley <gdgkirkley@gmail.com>
2019-08-02 13:51:11 -0700

    Updating package details, begun unit tests

--- R/control.R
+++ R/control.R
@@ -2,6 +2,22 @@ require(httr)
 require(jsonlite)
 require(magrittr)
 
+#' Send a Request to the Tessitura API
+#'
+#' @param host character
+#' @param basePath character
+#' @param method character
+#' @param credentials character
+#' @param request_type character
+#' @param data list
+#' @param flatten logical
+#'
+#' @return list
+#' @export
+#'
+#' @examples
+#'#' callTessi('mytessi.tessituranetwork.com/', 'TessituraService', '/Diagnostics/Status', 'mybase64credentials')
+
 callTessi <- function(host, basePath, method, credentials, request_type = "GET", data, flatten=TRUE){
 
   url <- paste0(host, basePath, method)
@@ -12,7 +28,7 @@ callTessi <- function(host, basePath, method, credentials, request_type = "GET",
 
     result <- httr::GET(
       url,
-      add_headers(.headers = c('Authorization' = credentials))
+      httr::add_headers(.headers = c('Authorization' = credentials))
       )
 
   } else if(request_type == "POST") {
@@ -20,9 +36,9 @@ callTessi <- function(host, basePath, method, credentials, request_type = "GET",
     if(typeof(data) != "list"){stop()}
   # Continue working on the try catch aspect of this function
     tryCatch({
-    result <- POST(
+    result <- httr::POST(
       url,
-      auth,
+      httr::add_headers(.headers = c('Authorization' = credentials)),
       encode = "json",
       body=data
     )}, warning = function(war) {
@@ -38,25 +54,37 @@ callTessi <- function(host, basePath, method, credentials, request_type = "GET",
     stop("There was a problem with your request type.")
   }
 
-  if(status_code(result) != 200){
+  if(httr::status_code(result) != 200){
     stop(
-      message("Your request returned status ", status_code(result)),
+      message("Your request returned status ", httr::status_code(result)),
       message("For more information, visit http://en.wikipedia.org/wiki/Http_error_codes"),
-      message("The message from the server was ", content(result))
+      message("The message from the server was ", httr::content(result))
       )
   }
 
   if(flatten == TRUE){
     return(
       result %>%
-      content("text") %>%
-      fromJSON(flatten=TRUE)
+      httr::content("text") %>%
+      jsonlite::fromJSON(flatten=TRUE)
     )
   }
 
   return(result)
 }
 
+#' Create Tessitura API Credentials
+#'
+#' @param username character
+#' @param usergroup character
+#' @param location character
+#' @param password character
+#'
+#' @return character
+#' @export
+#'
+#' @examples
+#' #' createCredentials(username="creif", usergroup="admin", location="MET95", password="impresario")
 createCredentials <- function(username, usergroup, location, password) {
   cred <- paste0(
       "Basic ", jsonlite::base64_enc(
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(tessituraR)
+
+test_check("tessituraR")
