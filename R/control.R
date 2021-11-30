c12fb8a7a889f9d1fe12a01bc5353f47b65f56a1
Gabe Kirkley <gdgkirkley@gmail.com>
2021-11-29 17:27:48 -0800

    Update to create TessituraService model

--- R/control.R
+++ R/control.R
@@ -3,7 +3,207 @@ require(jsonlite)
 require(magrittr)
 require(stringr)
 
-#' Send a Request to the Tessitura API
+#' A TessituraService class
+#'
+#' This is a more useable Tessitura Service class that can be implemented by
+#' further objects to create requests.
+#'
+#' @docType class
+#' @title TessituraService
+#' @field tessituraUrl Base Tessitura REST API url
+#' @field credentials Base64 encoded credentials - can be created with createCredentials()
+#' @field defaultHeaders Default headers for all requests
+#' @field timeout The maximum timeout for a request
+#' @field maxRetryAttempts The maximum number of times to retry a request before failing
+#' @export
+#'
+#' @examples
+#' TessituraService$new(
+#' tessituraUrl = "https://mytessi.tessituranetwork.com",
+#' credentials = createCredentials(username="creif", usergroup="admin", location="MET95", password="impresario")
+#' )
+TessituraService <- R6::R6Class(
+  classname = "TessituraService",
+  public = list(
+    tessituraUrl = NULL,
+    credentials = NULL,
+    defaultHeaders = NULL,
+    timeout = NULL,
+    maxRetryAttempts = NULL,
+
+    #' @description Constructor
+    #'
+    #' @param tessituraUrl Base Tessitura REST API url
+    #' @param credentials Base64 encoded credentials
+    #' @param defaultHeaders Default headers for all requests
+    #' @param timeout The maximum timeout for a request. Defaults to 5000.
+    #' @param maxRetryAttempts The maximum number of times to retry a request before failing. Defaults to 3.
+    #'
+    initialize = function(tessituraUrl, credentials, defaultHeaders = NULL, timeout = 5000, maxRetryAttempts = 3) {
+      if(!is.null(tessituraUrl)) {
+        self$tessituraUrl <- tessituraUrl
+      }
+
+      if(!is.null(credentials)) {
+        self$credentials <- credentials
+      }
+
+      if(!is.null(defaultHeaders)) {
+        self$defaultHeaders <- defaultHeaders
+      }
+
+      if (!is.null(timeout)) {
+        self$timeout <- timeout
+      }
+
+      if(!is.null(maxRetryAttempts)) {
+        self$maxRetryAttempts <- maxRetryAttempts
+      }
+    },
+
+    #'
+    #' @description Call a Tessitura endpoint with retries
+    #'
+    #' @param url The endpoint to request relative to the tessituraUrl
+    #' @param method The HTTP method
+    #' @param queryParams The query parameters for the request
+    #' @param headerParams Any additional headers to attach to the request
+    #' @param body The request body for POST and PUT methods
+    #' @param ... Any other parameters for the httr methods
+    #'
+    CallTessi = function(url, method, queryParams, headerParams, body, ...) {
+      retryStatusCodes <- c(500)
+      resp <- self$Execute(url, method, queryParams, headerParams, body, ...)
+      statusCode <- httr::status_code(resp)
+
+      if (is.null(self$maxRetryAttempts)) {
+        self$maxRetryAttempts = 3
+      }
+
+      for (i in 1 : self$maxRetryAttempts) {
+        if (statusCode %in% retryStatusCodes) {
+          Sys.sleep((2 ^ i) + stats::runif(n = 1, min = 0, max = 1))
+          resp <- self$Execute(url, method, queryParams, headerParams, body, ...)
+          statusCode <- httr::status_code(resp)
+        } else {
+          break
+        }
+      }
+
+      resp
+    },
+
+    #'
+    #' @description Execute a request
+    #'
+    #' @param url The endpoint to request relative to the tessituraUrl
+    #' @param method The HTTP method
+    #' @param queryParams The query parameters for the request
+    #' @param headerParams Any additional headers to attach to the request
+    #' @param body The request body for POST and PUT methods
+    #' @param ... Any other parameters for the httr methods
+    #'
+    Execute = function(url, method, queryParams, headerParams, body, ...) {
+      headers = httr::add_headers(c(headerParams, self$defaultHeaders))
+
+      if (method == "GET") {
+        httr::GET(url, query = queryParams, headers, self$timeout, ...)
+      } else if (method == "POST") {
+        httr::POST(url, query = queryParams, headers, body = body,
+                   httr::content_type("application/json"), ....)
+      } else if (method == "PUT") {
+        httr::PUT(url, query = queryParams, headers, body = body,
+                   httr::content_type("application/json"), ....)
+      } else if (method == "DELETE") {
+        httr::DELETE(url, query = queryParams, headers, self$timeout, ...)
+      } else {
+        error <- "HTTP Method must be GET, POST, PUT or DELETE"
+        stop(error)
+      }
+    },
+
+    #' @description Deserialize the content of api response to the given type.
+    #'
+    #' @param resp The response object
+    #' @param returnType The type of object to return
+    #' @param pkgEnv The environment to find the type in
+    #'
+    deserialize = function(resp, returnType, pkgEnv) {
+      respObj <- jsonlite::fromJSON(httr::content(resp, "text", encoding = "UTF-8"))
+      self$deserializeObj(respObj, returnType, pkgEnv)
+    },
+
+    #' @description Deserialize the response from jsonlite object based on the given type
+    #' by handling complex and nested types by iterating recursively
+    #' Example returnTypes will be like "array[integer]", "map(Performance)", "array[map(Performance)]", etc.,
+    #'
+    #' @param obj The object to deserialize
+    #' @param returnType The type of object to return
+    #' @param pkgEnv The environment to find the type in
+    #'
+    deserializeObj = function(obj, returnType, pkgEnv) {
+      returnObj <- NULL
+      primitiveTypes <- c("character", "numeric", "integer", "logical", "complex")
+
+      if (startsWith(returnType, "map(")) {
+        innerReturnType <- regmatches(returnType, regexec(pattern = "map\\((.*)\\)", returnType))[[1]][2]
+        returnObj <- lapply(names(obj), function(name) {
+          self$deserializeObj(obj[[name]], innerReturnType, pkgEnv)
+        })
+        names(returnObj) <- names(obj)
+      }
+
+      else if (startsWith(returnType, "array[")) {
+        innerReturnType <- regmatches(returnType, regexec(pattern = "array\\[(.*)\\]", returnType))[[1]][2]
+        if (c(innerReturnType) %in% primitiveTypes) {
+          returnObj <- vector("list", length = length(obj))
+          if (length(obj) > 0) {
+            for (row in 1:length(obj)) {
+              returnObj[[row]] <- self$deserializeObj(obj[row], innerReturnType, pkgEnv)
+            }
+          }
+        } else {
+          if(!is.null(nrow(obj))){
+            returnObj <- vector("list", length = nrow(obj))
+            if (nrow(obj) > 0) {
+              for (row in 1:nrow(obj)) {
+                returnObj[[row]] <- self$deserializeObj(obj[row, , drop = FALSE], innerReturnType, pkgEnv)
+              }
+            }
+          }
+        }
+      }
+
+      else if (exists(returnType, pkgEnv) && !(c(returnType) %in% primitiveTypes)) {
+        returnType <- get(returnType, envir = as.environment(pkgEnv))
+        returnObj <- returnType$new()
+        returnObj$fromJSON(
+          jsonlite::toJSON(obj, digits = NA, auto_unbox = TRUE)
+        )
+      }
+
+      else {
+        returnObj <- obj
+      }
+      returnObj
+    }
+  ),
+)
+
+#' Create a new Tessitura Service object
+#' @param tessituraUrl The base url for your Tessitura REST API
+#' @param credentials A base64 encded character string. Can be created with createCredentials()
+#'
+#' @return a TessituraService S4 object
+#' @export
+#'
+createTessituraService = function(tessituraUrl, credentials) {
+  return(
+    TessituraService$new(tessituraUrl, credentials)
+  )
+}
+
+#' Send a Request to the Tessitura API - Deprecated
 #'
 #' @param host The host name for your Tessitura API
 #' @param basePath The base path for your Tessitura API. eg., TessituraService
